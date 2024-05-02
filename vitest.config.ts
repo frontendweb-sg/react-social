7df2b0b0f5dfeff1d653a56fd3bef9463b5a59af
frontendweb-sg <frontendweb.sg@gmail.com>
2024-05-02 12:29:36 +0530

    setup testing environment

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { fileURLToPath } from "node:url";
+import viteConfig from "./vite.config";
+import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "e2e/**"],
+      root: fileURLToPath(new URL("./", import.meta.url)),
+    },
+  })
+);
